972d25aa2f0db9701e0017b2935f239f181b17d6
Diego Parra <dp@linkfire.com>
2023-07-15 10:58:57 -0400

    feat: updates

--- main.go
+++ main.go
@@ -3,11 +3,11 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"path/filepath"
 
 	"github.com/go-chi/chi/v5"
 
 	"github.com/diegoparra/calhoun/controllers"
+	"github.com/diegoparra/calhoun/templates"
 	"github.com/diegoparra/calhoun/views"
 )
 
@@ -15,13 +15,13 @@ func main() {
 	r := chi.NewRouter()
 
 	r.Get("/", controllers.StaticHandler(
-		views.Must(views.Parse(filepath.Join("templates", "home.html")))))
+		views.Must(views.ParseFS(templates.FS, "home.html"))))
 
 	r.Get("/contact", controllers.StaticHandler(
-		views.Must(views.Parse(filepath.Join("templates", "contact.html")))))
+		views.Must(views.ParseFS(templates.FS, "contact.html"))))
 
 	r.Get("/faq", controllers.StaticHandler(
-		views.Must(views.Parse(filepath.Join("templates", "faq.html")))))
+		views.Must(views.ParseFS(templates.FS, "faq.html"))))
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
