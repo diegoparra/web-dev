fbcb33762537876cb523865d29160ada20395b49
Diego Parra <diegoparraferreira@gmail.com>
2023-07-18 07:59:18 -0400

    updates

--- controllers/static.go
+++ controllers/static.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"html/template"
 	"net/http"
 
 	"github.com/diegoparra/calhoun/views"
@@ -9,5 +10,29 @@ import (
 func StaticHandler(tmpl views.Template) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		tmpl.Execute(w, nil)
+	}
+}
+
+func FAQ(tmpl views.Template) http.HandlerFunc {
+	questios := []struct {
+		Question string
+		Answer   template.HTML
+	}{
+		{
+			Question: "Is there a free version?",
+			Answer:   "Yes, it has a free version.",
+		},
+		{
+			Question: "What are the support hours?",
+			Answer:   "we have support staff answering email 27/7",
+		},
+		{
+			Question: "How do I contact support?",
+			Answer:   `Email us - <a href="mailto:diegoparraferreira@gmail.com">support@dpferreira.com.br</a>`,
+		},
+	}
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		tmpl.Execute(w, questios)
 	}
 }
