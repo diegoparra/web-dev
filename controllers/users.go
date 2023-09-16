b39460c451ea11adcad675304d0c2c1a27eb8cb9
Diego Parra <dp@linkfire.com>
2023-09-16 08:37:36 -0400

    update: printing cookies

--- controllers/users.go
+++ controllers/users.go
@@ -63,5 +63,25 @@ func (u Users) ProcessSignIn(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Something went wrong", http.StatusInternalServerError)
 		return
 	}
+
+	cookie := http.Cookie{
+		Name:  "email",
+		Value: user.Email,
+		Path:  "/",
+	}
+
+	http.SetCookie(w, &cookie)
+
 	fmt.Fprintf(w, "User Authenticated: %+v", user)
+}
+
+func (u Users) CurrentUser(w http.ResponseWriter, r *http.Request) {
+	email, err := r.Cookie("email")
+	if err != nil {
+		fmt.Fprint(w, "could not get the cookie")
+		return
+	}
+
+	fmt.Fprintf(w, "Email cookie: %s\n", email.Value)
+	fmt.Fprintf(w, "Headers: %v\n", r.Header)
 }
