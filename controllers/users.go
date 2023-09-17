8b31a813903aaf82da63959f5715b69b9c0acd52
Diego Parra <dp@linkfire.com>
2023-09-17 09:52:56 -0400

    update: remove js cookies access

--- controllers/users.go
+++ controllers/users.go
@@ -65,9 +65,10 @@ func (u Users) ProcessSignIn(w http.ResponseWriter, r *http.Request) {
 	}
 
 	cookie := http.Cookie{
-		Name:  "email",
-		Value: user.Email,
-		Path:  "/",
+		Name:     "email",
+		Value:    user.Email,
+		Path:     "/",
+		HttpOnly: true,
 	}
 
 	http.SetCookie(w, &cookie)
