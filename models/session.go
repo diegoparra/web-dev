b70da120da5aa1d48acff0b98a9bdc7c0abde484
Diego Parra <dp@linkfire.com>
2023-10-02 17:01:10 -0400

    updates

--- models/session.go
+++ models/session.go
@@ -1,7 +1,26 @@
 package models
 
+import "database/sql"
+
 type Session struct {
-	ID        int
-	UserID    int
+	ID     int
+	UserID int
+	// Token is only set when crating a new session
+	// this will be left empty, as we onlt store the hash of a session Token
+	// in our database and wen can't reverse it into a raw token.
+	Token     string
 	TokenHash string
+}
+
+type SessionService struct {
+	DB *sql.DB
+}
+
+func (ss *SessionService) Create(userID int) (*Session, error) {
+	// TODO: Create a session token
+	return nil, nil
+}
+
+func (ss *SessionService) User(token string) (*User, error) {
+	return nil, nil
 }
